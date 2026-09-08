81f3ecf4a3e988dfa05d887d7859e83c8ecc04c8
Kyriel Abad <abadkyriel@gmail.com>
2026-09-09 01:24:14 +0800

    guix: first versions of configs

---
+++ home-config.scm
@@ -0,0 +1,77 @@
+;; This is a sample Guix Home configuration which can help setup your
+;; home directory in the same declarative manner as Guix System.
+;; For more information, see the Home Configuration section of the manual.
+(define-module (guix-home-config)
+  #:use-module (guix gexp)
+  #:use-module (gnu packages)
+  #:use-module (gnu home)
+  #:use-module (gnu home services)
+  #:use-module (gnu home services desktop)
+  #:use-module (gnu home services fontutils)
+  #:use-module (gnu home services shells)
+  #:use-module (gnu home services sound)
+  #:use-module (gnu home services ssh)
+  #:use-module (gnu services)
+  #:use-module (gnu system shadow)
+  #:use-module (nongnu packages nvidia)
+  #:use-module (nongnu packages mozilla)
+  #:use-module (claude-code-guix packages claude-code)
+  #:use-module (pi-guix packages pi))
+
+(define home-config
+  (home-environment
+    (packages 
+      (append
+        (list claude-code pi-coding-agent)
+        (map (compose replace-mesa specification->package)
+          (list "pavucontrol"
+	        "foot"
+	        "emacs"
+	        "neovim"
+	        "firefox"
+	        "prusa-slicer"
+	        "telegram-desktop"
+	        "font-iosevka"
+	        "font-google-noto-emoji"))))
+    (services
+      (append
+        (list
+          ;; Uncomment the shell you wish to use for your user:
+          ;(service home-bash-service-type)
+          ;(service home-fish-service-type)
+          (service home-zsh-service-type)
+	  (service home-ssh-agent-service-type)
+	  (service home-openssh-service-type
+		   (home-openssh-configuration
+		     (add-keys-to-agent "yes")))
+	  (service home-dbus-service-type)
+	  (service home-pipewire-service-type
+		   (home-pipewire-configuration
+		     (enable-pulseaudio? #t)))
+         (simple-service 'font-prefs
+                      home-fontconfig-service-type
+                      (list
+                        '(alias
+                           (family "monospace")
+                           (prefer (family "Iosevka")
+                                   (family "Noto Color Emoji")))))
+
+	  (simple-service 'foot-config
+			  home-xdg-configuration-files-service-type
+			  (list (list "foot/foot.ini"
+				      (plain-file "foot.ini"
+"[main]
+font=Iosevka Term:size=14
+"))))
+
+          (service home-files-service-type
+           `((".guile" ,%default-dotguile)
+             (".Xdefaults" ,%default-xdefaults)))
+
+          (service home-xdg-configuration-files-service-type
+           `(("gdb/gdbinit" ,%default-gdbinit)
+             ("nano/nanorc" ,%default-nanorc))))
+
+        %base-home-services))))
+
+home-config
---
+++ system-config.scm
@@ -0,0 +1,118 @@
+(use-modules (gnu)
+	     (gnu services base)
+	     (gnu packages display-managers)
+	     (gnu packages glib)
+	     (gnu packages window-management)
+	     (noctalia)
+	     (nongnu packages nvidia)
+	     (nongnu services nvidia)
+	     (nonguix transformations)
+	     (nongnu packages linux)
+	     (nongnu system linux-initrd))
+
+(use-service-modules cups desktop networking ssh xorg)
+
+((nonguix-transformation-nvidia #:driver nvda)
+ (operating-system
+   (kernel linux)
+   (initrd microcode-initrd)
+   (firmware (cons* iwlwifi-firmware 
+   		    linux-firmware
+		    %base-firmware))
+   (kernel-arguments
+     (cons* "modprobe.blacklist=nouveau,amdgpu"
+	    "nvidia_drm.modeset=1"
+            "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+	    %default-kernel-arguments))
+   (kernel-loadable-modules (list nvidia-module #;nvidia-driver))
+
+   (locale "en_SG.utf8")
+   (timezone "Asia/Singapore")
+   (keyboard-layout (keyboard-layout "us"))
+   (host-name "flatwhite")
+
+   ;; The list of user accounts ('root' is implicit).
+   (users (cons* (user-account
+                   (name "skybound")
+                   (comment "Kyriel Abad")
+                   (group "users")
+                   (home-directory "/home/skybound")
+                   (supplementary-groups '("wheel" "netdev" "audio" "video")))
+                 %base-user-accounts))
+
+   ;; Packages installed system-wide.  Users can also install packages
+   ;; under their own account: use 'guix search KEYWORD' to search
+   ;; for packages and 'guix install PACKAGE' to install a package.
+   (packages 
+     (append (map specification->package
+		  (list #;"llama-cpp"
+		        "niri"
+ 	                "foot"
+ 	                "noctalia-git"
+                        "ranger"
+                        "btop"
+                        "tmux"
+                        "git"
+                        "vim"
+			"openssh"))
+     %base-packages))
+
+   ;; Below is the list of system services.  To search for available
+   ;; services, run 'guix system search KEYWORD' in a terminal.
+   (services
+     (cons* (service nvidia-service-type)
+	    (service pam-limits-service-type
+		     (list (pam-limits-entry "@realtime" 'both 'rtprio 99)
+			   (pam-limits-entry "@realtime" 'both 'memlock 'unlimited)))
+	    (service greetd-service-type
+		     (greetd-configuration
+		       (greeter-supplementary-groups (list "video" "input"))
+		       (terminals
+			 (list
+			   (greetd-terminal-configuration
+			     (terminal-vt "7")
+			     (terminal-switch #t)
+			     (default-session-command
+			       (greetd-agreety-session
+				 (command (greetd-user-session
+			           (command (file-append dbus "/bin/dbus-run-session"))
+				   (command-args (list (file-append niri "/bin/niri") "--session")))))))))))
+   	    (modify-services %desktop-services
+			     (delete gdm-service-type)
+			     #;(alsa-service-type config => (alsa-configuration (pulseaudio? #f))))))
+
+   (bootloader (bootloader-configuration
+                 (bootloader grub-efi-bootloader)
+                 (targets '("/boot/efi"))
+		 (menu-entries 
+		   (list (menu-entry
+			   (label "NixOS")
+			   (chain-loader "/EFI/NixOS-boot/grubx64.efi")))) 
+			   (keyboard-layout keyboard-layout)))
+
+   ;; The list of file systems that get "mounted".  The unique
+   ;; file system identifiers there ("UUIDs") can be obtained
+   ;; by running 'blkid' in a terminal.
+   (file-systems (cons* (file-system
+                          (mount-point "/slowdisk")
+                          (device (uuid
+                                   "5296c0e9-e5cd-4679-9c06-e6b976f61fe2"
+                                   'ext4))
+                          (type "ext4"))
+                        (file-system
+                          (mount-point "/fastdisk")
+                          (device (uuid
+                                   "d73e4110-48d2-4f5a-aff9-c0dabb29452d"
+                                   'ext4))
+                          (type "ext4"))
+                        (file-system
+                          (mount-point "/")
+                          (device (uuid
+                                   "eecfbb00-763a-4078-9619-9d60fa071508"
+                                   'ext4))
+                          (type "ext4"))
+                        (file-system
+                          (mount-point "/boot/efi")
+                          (device (uuid "CB77-1709"
+                                        'fat32))
+                          (type "vfat")) %base-file-systems))))
