262afb308575ab8e2d7af6e9a235305e7d4d715c
Kyriel Abad <abadkyriel@gmail.com>
2026-09-09 09:41:21 +0800

    prusaslicer: fix login, 3d render

--- home-config.scm
+++ home-config.scm
@@ -11,6 +11,7 @@
   #:use-module (gnu home services shells)
   #:use-module (gnu home services sound)
   #:use-module (gnu home services ssh)
+  #:use-module (gnu home services xdg)
   #:use-module (gnu services)
   #:use-module (gnu system shadow)
   #:use-module (nongnu packages nvidia)
@@ -22,14 +23,18 @@
     (packages 
       (append
         (list claude-code pi-coding-agent)
+        ;; kept off replace-mesa (it corrupts prusa-slicer's viewport); glib-networking backs its login webview's TLS
+        (map specification->package
+          (list "prusa-slicer"
+                "glib-networking"))
         (map (compose replace-mesa specification->package)
           (list "pavucontrol"
+	        "xdg-utils"
 	        "alsa-utils"
 	        "foot"
 	        "emacs"
 	        "neovim"
 	        "firefox"
-	        "prusa-slicer"
 	        "telegram-desktop"
 	        "font-iosevka"
 	        "font-google-noto-emoji"))))
@@ -70,6 +75,31 @@
 			  home-xdg-configuration-files-service-type
 			  (list (list "niri/config.kdl"
 				      (local-file ".guix/niri.kdl"))))
+
+          ;; dummy proxy resolver stops libproxy crashing PrusaSlicer's WebKit network process
+          (simple-service 'webkit-login-env
+                          home-environment-variables-service-type
+                          '(("GIO_EXTRA_MODULES" .
+                             "$HOME/.guix-home/profile/lib/gio/modules:/run/current-system/profile/lib/gio/modules")
+                            ("GIO_USE_PROXY_RESOLVER" . "dummy")))
+
+          (service home-xdg-mime-applications-service-type
+                   (home-xdg-mime-applications-configuration
+                     (default
+                       '((x-scheme-handler/http        . firefox.desktop)
+                         (x-scheme-handler/https       . firefox.desktop)
+                         (text/html                    . firefox.desktop)
+                         (x-scheme-handler/prusaslicer . prusaslicer-url.desktop)))
+                     (desktop-entries
+                       (list
+                         (xdg-desktop-entry
+                           (file "prusaslicer-url")
+                           (name "PrusaSlicer URL Handler")
+                           (type 'application)
+                           (config
+                             '((exec . "prusa-slicer %u")
+                               (icon . "PrusaSlicer")
+                               (terminal . #f))))))))
 
           (service home-files-service-type
            `((".guile" ,%default-dotguile)
